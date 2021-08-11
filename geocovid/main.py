1f4ae4924dd0e2026a014c1958dbff5e837d9dd2
Ariel Wolfmann <ariel.wolfmann@rappi.com>
2021-08-11 14:49:15 -0300

    main.py WIP

--- geocovid/main.py
+++ geocovid/main.py
@@ -1,8 +1,18 @@
 """Main file CLI for run the simulation of the Geo Covid Model."""
 
 import click
+import glob
+import tarfile
+import logging
+import os
 
 from geocovid.model import GeoCovidModel
+from geocovid.utils import start_spark
+from geocovid.data_pipeline import extract_data_spark, transform_data_spark
+
+
+logger = logging.getLogger(__name__)
+
 
 @click.command()
 def main():
@@ -12,20 +22,24 @@ def main():
     ----------
 
     """
-    cm = GeoCovidModel()
-    # listar todos los archivos
-    # iterar sobre ellos, extrayendolo
-    files = os.path(path)
-    for f in files:
-        df = read(f)
-        df = preprocess_time(df)
-        df = preprocess_id(df)
-        gdf = preprocess_geo(df)
-        gdf_agg = preprocess_aggregate(gdf)
-        hours = gdf_agg.hour.unique()
+    gcm = GeoCovidModel()
+    path_gz = '/Users/awolfmann/Documents/data_eng_challenge/Data Engineer - Covid/data/iso_date=2020-06-01.tar.gz'
+    spark = start_spark()
+    with tarfile.open(path_gz, 'r:gz') as tf:
+        for m in tf.getmembers():
+            if m.isdir():
+                logger.info(' dirname %s' % m.name)
+                sdf = extract_data_spark(os.path.basename(m.name), spark)
+                logger.info('extracted data')
+                gdf = transform_data_spark(sdf, spark)
+                logger.info('transformed data')
+                hours = gdf.index.levels[0]
 
-        for h in hours:
-            cm.step(gdf_agg[gdf_agg['hour'] == h])
+                for h in hours:
+                    gcm.step(gdf.loc[h, :])
+                    logger.info('data collector %s' % gcm.datacollector.model_vars)
+        tf.close()
+
 
 if __name__ == "__main__":
     main()
