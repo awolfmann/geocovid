c92d7fd05894b4b7e2a2d3a4508e55b9dc6136f5
Ariel Wolfmann <ariel.wolfmann@rappi.com>
2021-08-13 11:28:34 -0300

    add steps per day constant

--- geocovid/constants.py
+++ geocovid/constants.py
@@ -7,6 +7,7 @@ TREATMENT_PERIOD = 14
 EXPOSURE_DISTANCE = 0.0001
 INIT_INFECTED = 100
 MIN_DEATH_PERIOD = 1
+STEPS_PER_DAY = 24
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(ROOT_DIR, "data/")
