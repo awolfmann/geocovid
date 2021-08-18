287e8802034fac34ed63ba4ea30111adc3e7e922
Ariel Wolfmann <ariel.wolfmann@rappi.com>
2021-08-18 14:54:13 -0300

    utils minor fix

--- geocovid/utils.py
+++ geocovid/utils.py
@@ -1,13 +1,10 @@
 """Utils module."""
 from pyspark.sql import SparkSession
-from sedona.utils import SedonaKryoRegistrator, KryoSerializer
 from sedona.register import SedonaRegistrator
+from sedona.utils import KryoSerializer, SedonaKryoRegistrator
 
 
-def start_spark(
-    app_name: str = 'Sedona App',
-    master: str ='local[*]'
-) -> SparkSession:
+def start_spark(app_name: str = "Sedona App", master: str = "local[*]") -> SparkSession:
     """
     Start Spark session.
 
@@ -20,14 +17,17 @@ def start_spark(
     -------
     spark session instance
     """
-    spark = SparkSession.\
-        builder.\
-        master(master).\
-        appName(app_name).\
-        config("spark.serializer", KryoSerializer.getName).\
-        config("spark.kryo.registrator", SedonaKryoRegistrator.getName) .\
-        config("spark.jars.packages", """org.apache.sedona:sedona-python-adapter-3.0_2.12:1.0.1-incubating,org.datasyslab:geotools-wrapper:geotools-24.0""") .\
-        getOrCreate()
+    spark = (
+        SparkSession.builder.master(master)
+        .appName(app_name)
+        .config("spark.serializer", KryoSerializer.getName)
+        .config("spark.kryo.registrator", SedonaKryoRegistrator.getName)
+        .config(
+            "spark.jars.packages",
+            """org.apache.sedona:sedona-python-adapter-3.0_2.12:1.0.1-incubating,org.datasyslab:geotools-wrapper:geotools-24.0""",
+        )
+        .getOrCreate()
+    )
 
     SedonaRegistrator.registerAll(spark)
 
