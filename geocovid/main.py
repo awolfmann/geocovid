20019aaf143cc1f3ca40b8e0ca0192a519481fd8
Ariel Wolfmann <ariel.wolfmann@rappi.com>
2021-08-21 09:27:34 -0300

    improvements on main

--- geocovid/main.py
+++ geocovid/main.py
@@ -1,21 +1,20 @@
 """Main file CLI for run the simulation of the Geo Covid Model."""
 
 import glob
-import json
 import logging
 import os
 import tarfile
 
 import click
 
-from geocovid.constants import DATA_DIR, TMP_DIR
+from geocovid.constants import DATA_DIR, ROOT_DIR, TMP_DIR
 from geocovid.data_pipeline import extract_data_spark, transform_data_spark
 from geocovid.model import GeoCovidModel
 from geocovid.utils import start_spark
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logging.info("test")
+LOG_FMT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO, format=LOG_FMT)
+logger = logging.getLogger(__name__)
 
 
 @click.command()
@@ -30,25 +29,34 @@ def main():
     files = glob.glob(os.path.join(DATA_DIR, "*.tar.gz"))
     sorted_files = sorted(files)
     spark = start_spark()
-    for file in sorted_files[:3]:
+    for file in sorted_files:
         with tarfile.open(file, "r:gz") as tfile:
-            path = os.path.join(TMP_DIR, os.path.basename(file).split(".")[0])
-            tfile.extractall(path=path, members=tfile)
-            logger.info("dirname %s", path)
-            sdf = extract_data_spark(path, spark)
-            logger.info("extracted data")
-            gdf = transform_data_spark(sdf, spark)
-            logger.info("transformed data")
-            hours = gdf.index.levels[0]
+            try:
+                path = os.path.join(TMP_DIR, os.path.basename(file).split(".")[0])
+                tfile.extractall(path=path, members=tfile)
+                logger.info("dirname %s", path)
+                sdf = extract_data_spark(path, spark)
+                logger.info("extracted data")
+                gdf = transform_data_spark(sdf, spark)
+                logger.info("transformed data")
+                hours = gdf.index.levels[0]
 
-            for hour in hours:
-                gcm.step(gdf.loc[hour, :])
-                logger.info("data collector %s", gcm.datacollector.model_vars)
+                for hour in hours:
+                    gcm.step(gdf.loc[hour, :])
+                    logger.info("data collector %s", gcm.datacollector.model_vars)
+
+            except EOFError as eof:
+                logger.info("FAILED dirname %s", eof)
+
             tfile.close()
 
-    with open("results.json", "w") as outjson:
-        logger.info("dumping results to json")
-        json.dump(gcm.datacollector.model_vars, outjson)
+    logger.info("dumping model vars results to json")
+    model_vars_df = gcm.datacollector.get_model_vars_dataframe()
+    model_vars_df.to_json(os.path.join(ROOT_DIR, "results_model.json"))
+
+    logger.info("dumping agent vars results to json")
+    agent_vars_df = gcm.datacollector.get_agent_vars_dataframe()
+    agent_vars_df.to_json(os.path.join(ROOT_DIR, "results_agents.json"), orient="table")
 
 
 if __name__ == "__main__":
